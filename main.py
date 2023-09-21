fe9075bffd762b6ce97dd662251a92edb2ee24aa
kalivhack <96864261+kalivhack@users.noreply.github.com>
2023-09-21 18:48:23 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,42 @@
+#!/usr/bin/python
 import itertools
-import sys
-from string import digits, punctuation, ascii_letters
-import time
-# from bs4 import BeautifulSoup
-# import requests
-from datetime import datetime
+import argparse
+from colorama import Fore
+import string
 
-l = 0
+# Argparse
+parser = argparse.ArgumentParser(prog = 'Passsword List Generator For BruteForce')
+parser.add_argument('-min', '--min', help='Min Length of Password (default = 3)', dest='min', type=int, default='3')
+parser.add_argument('-max', '--max', help='Max Length of Password', dest='max', type=int, required=True)
+parser.add_argument('-t', '--type', help='Letters = 1, Numbers = 2, Symbols = 3 (ex. 1/3)', dest='type', default='1/2/3', required=True)
+parser.add_argument('-f', '--file', help='Name Of File To Save Password', dest='file', default='passwords.txt')
 
-syimvols = [digits, punctuation, ascii_letters]
-# print(len(sys.argv))
-if len(sys.argv) < 2:
-    file_name = "pass.txt"
+# Variables
+file_name = parser.parse_args().file
+let_type = parser.parse_args().type
+min_length = parser.parse_args().min
+max_length = parser.parse_args().max
+symbols = [string.ascii_letters, string.digits, string.punctuation]
 
-elif len(sys.argv) >= 2:
-    file_name = sys.argv[1]
-
-
-def bruetforce_exel():
-    # rl = BeautifulSoup(src, "lxml")
-    print("*****bad_Brut*****")
-    qu = input("start?(y n): ")
-    if qu == "n":
-        exit()
+# Generate Passwords
+def make_passlist(max_length, min_length):
     try:
-        
-        brut_length = input("input nmu_1 && num-2: ")
-        brut_length = [int(item) for item in brut_length.split('-')]
+        types = let_type.split('/')
+        password_list = "".join([symbols[int(i) - 1] for i in types])
 
+        with open(file_name, 'w+') as file:
+            print(f'{Fore.LIGHTGREEN_EX}Starting Password Generation:\n')
+            for length in range(int(min_length), int(max_length) + 1):
+                for password in itertools.product(password_list, repeat=length):
+                    file.write(''.join(password)+ '\n')
 
-    except:
-        print("try again")
-        bruetforce_exel()
+        print(f"{Fore.LIGHTMAGENTA_EX}Passwords Was Saved In {file_name}")
 
+    except Exception as ex:
+        print(f' {Fore.LIGHTRED_EX}Error Happened exiting...')
 
-    r = int(input("if only number enter 0\nif only text type 1"
-        "\nif only !@# type 2\nif all of this type 3\n4 your variant\n"))
-    if r > 4:
-        exit()
-    if r == 4:
-        r_5 = str(input("type here: "))
+def main():
+    make_passlist(min_length=min_length, max_length=max_length)
 
-    if r == 0:
-        l = syimvols[0]
-    elif r == 1:
-        l = syimvols[2]
-    elif r == 2:
-        l = syimvols[1]
-    elif r == 3:
-        l = syimvols[0] + syimvols[1] + syimvols[2]
-    elif r == 4:
-        j = [int(item) for item in r_5.split("+")]
-        l = syimvols[j[0]] + syimvols[j[1]]
-
-
-
-    star_time = time.time()
-    count = 0
-    print(f"Started at {datetime.utcfromtimestamp(time.time()).strftime('%H:%M:%S')}")
-#   brouteforce procces
-    print(brut_length[0], brut_length[1] + 1)
-    for i in range(brut_length[0], brut_length[1] + 1):
-        with open(file_name, "w") as file:
-            for password in itertools.product(l, repeat=i):
-                password = "".join(password)     
-                
-                file.write(password + '\n')
-                print(password)
-    print(f"file was saved with name {file_name}")
-
-bruetforce_exel()
-
+if __name__ == '__main__':
+    main()
